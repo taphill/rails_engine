07351fcca2759a6073041765fb1f94c629e0a4f6
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-12 17:27:54 -0600

    Add spec for Merchant model

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,7 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:name) }
+  end
+
+  describe 'relationships' do
+    it { is_expected.to have_many(:items) }
+    it { is_expected.to have_many(:invoices) }
+    it { is_expected.to have_many(:customers).through(:invoices) }
   end
 end
