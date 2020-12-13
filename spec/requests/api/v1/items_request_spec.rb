20cf609b8eb0b62e34763b16bafd26af8af3b453
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-13 17:23:03 -0600

    Add spec for GET /items/:id request

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Api/V1/Items request', type: :request do
   describe 'GET /items' do
     before do
       create_list(:item, 3)
-      get '/api/v1/items'
+      get api_v1_items_path
     end
 
     let(:json_body) { JSON.parse(response.body, symbolize_names: true) }
@@ -33,6 +33,35 @@ RSpec.describe 'Api/V1/Items request', type: :request do
         expect(item[:attributes]).to have_key(:merchant_id)
         expect(item[:attributes][:merchant_id]).to be_a(Integer)
       end
+    end
+  end
+
+  describe 'GET /items/:id' do
+    let(:item) { create(:item) }
+    let(:json_body) { JSON.parse(response.body, symbolize_names: true) }
+
+    before { get api_v1_item_path(item.id) }
+
+    it { expect(response.status).to eq(200) }
+
+    it { expect(json_body).to be_a(Hash) }
+    it { expect(json_body).to have_key(:data) }
+    it { expect(json_body[:data]).to be_a(Hash) }
+    it { expect(json_body[:data]).to have_key(:attributes) }
+    it { expect(json_body[:data][:attributes]).to be_a(Hash) }
+
+    it 'has item attributes' do
+      expect(json_body[:data][:attributes]).to have_key(:name)
+      expect(json_body[:data][:attributes][:name]).to be_a(String)
+
+      expect(json_body[:data][:attributes]).to have_key(:description)
+      expect(json_body[:data][:attributes][:description]).to be_a(String)
+
+      expect(json_body[:data][:attributes]).to have_key(:unit_price)
+      expect(json_body[:data][:attributes][:unit_price]).to be_a(Float)
+
+      expect(json_body[:data][:attributes]).to have_key(:merchant_id)
+      expect(json_body[:data][:attributes][:merchant_id]).to be_a(Integer)
     end
   end
 end
