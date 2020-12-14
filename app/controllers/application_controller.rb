3d90cbb05c3d900d2b06b9dc83b92e0df36afd86
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-13 18:41:52 -0600

    Add record invalid helper method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@
 
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
 
   private
 
   def record_not_found
     render json: { message: 'Not Found' }, status: 404
+  end
+
+  def record_invalid(error)
+    render json: { message: error.message }, status: 403
   end
 end
