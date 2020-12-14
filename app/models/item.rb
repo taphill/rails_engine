1dad837f540eba99fe001f00dc8ae317a50beb41
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-14 16:18:21 -0600

    Update Item model validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Item < ApplicationRecord
-  validates :name, :description, :unit_price, presence: true
+  validates :name, :description, :unit_price, :merchant_id, presence: true
 
   belongs_to :merchant
   has_many :invoice_items, dependent: :destroy
