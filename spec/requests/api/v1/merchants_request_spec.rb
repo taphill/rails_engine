05aaf5d0b02283be6b2d77275483b2b9cb65d9ac
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-14 17:11:25 -0600

    Add tests for GET /merchants/:id request

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -27,12 +27,35 @@ RSpec.describe 'Api/V1/Merchants request', type: :request do
     end
   end
 
-  describe 'valid GET /items/:id request' do
+  describe 'valid GET /merchants/:id request' do
     let(:merchant) { create(:merchant) }
     let(:json_body) { JSON.parse(response.body, symbolize_names: true) }
 
     before { get api_v1_merchant_path(merchant.id) }
 
     it { expect(response.status).to eq(200) }
+
+    it { expect(json_body).to be_a(Hash) }
+    it { expect(json_body).to have_key(:data) }
+    it { expect(json_body[:data]).to be_a(Hash) }
+    it { expect(json_body[:data]).to have_key(:attributes) }
+    it { expect(json_body[:data][:attributes]).to be_a(Hash) }
+
+    it 'has item attributes' do
+      expect(json_body[:data][:attributes]).to have_key(:name)
+      expect(json_body[:data][:attributes][:name]).to be_a(String)
+    end
+  end
+
+  describe 'invalid GET /merchants/:id request' do
+    let(:json_body) { JSON.parse(response.body, symbolize_names: true) }
+
+    it 'returns a 404' do
+      get api_v1_merchant_path(4)
+
+      expect(response.status).to eq(404)
+      expect(json_body).to have_key(:message)
+      expect(json_body[:message]).to eq('Not Found')
+    end
   end
 end
