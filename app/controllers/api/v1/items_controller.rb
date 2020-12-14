6163e18b9c8daade7ac8dcd8265589f4d6ac4bec
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-13 18:41:27 -0600

    Add create action to api/v1 Items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -10,6 +10,16 @@ module Api
       def show
         render json: ItemSerializer.new(Item.find(params[:id]))
       end
+
+      def create
+        render json: ItemSerializer.new(Item.create!(item_params))
+      end
+
+      private
+
+      def item_params
+        params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+      end
     end
   end
 end
