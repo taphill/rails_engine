8b2487774a54583f1bc67be4ff10c92de9f3c78a
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-15 11:16:41 -0600

    Add tests for POST and DELETE api/v1 merchants requests

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -58,4 +58,54 @@ RSpec.describe 'Api/V1/Merchants request', type: :request do
       expect(json_body[:message]).to eq('Not Found')
     end
   end
+
+  describe 'valid POST /merchants request' do
+    let(:merchant_params) { { name: 'Great Item' } }
+    let(:headers) { { 'CONTENT_TYPE' => 'application/json' } }
+
+    before do
+      post api_v1_merchants_path, headers: headers, params: JSON.generate(merchant_params)
+    end
+
+    it { expect(response.status).to eq(200) }
+    it { expect(Merchant.last.name).to eq(merchant_params[:name]) }
+  end
+
+  describe 'no name in POST /merchants request' do
+    let(:merchant_params) { { name: '' } }
+
+    let(:headers) { { 'CONTENT_TYPE' => 'application/json' } }
+    let(:json_body) { JSON.parse(response.body, symbolize_names: true) }
+
+    it 'returns a 403' do
+      post api_v1_merchants_path, headers: headers, params: JSON.generate(merchant_params)
+
+      expect(response.status).to eq(403)
+      expect(json_body).to have_key(:message)
+      expect(json_body[:message]).to eq("Validation failed: Name can't be blank")
+    end
+  end
+
+  describe 'valid DELETE /merchants/:id request' do
+    let!(:merchant) { create(:merchant) }
+
+    it { expect(Merchant.count).to eq(1) }
+    it { expect { delete api_v1_merchant_path(merchant.id) }.to change(Merchant, :count).by(-1) }
+
+    it 'deletes the item' do
+      delete api_v1_merchant_path(merchant.id)
+      expect(response.status).to eq(204)
+      expect(Item.count).to eq(0)
+    end
+  end
+
+  describe 'invalid DELETE /merchants/:id request' do
+    let(:json_body) { JSON.parse(response.body, symbolize_names: true) }
+
+    it 'returns a 404' do
+      delete api_v1_merchant_path(4)
+      expect(response.status).to eq(404)
+      expect(json_body[:message]).to eq('Not Found')
+    end
+  end
 end
