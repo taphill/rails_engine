945419952d737b584b7d4b3d78d17021e130840c
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-16 17:25:43 -0600

    Update Merchant search controller and model

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -8,7 +8,9 @@ module Api
           render json: MerchantSerializer.new(merchants)
         end
 
-        def show; end
+        def show
+          render json: MerchantSerializer.new(merchant)
+        end
 
         private
 
@@ -17,6 +19,14 @@ module Api
           return Merchant.find_all_by_created_at(params[:created_at]) if params[:created_at]
           return Merchant.find_all_by_updated_at(params[:updated_at]) if params[:updated_at]
         end
+
+        # rubocop:disable Rails/DynamicFindBy
+        def merchant
+          return Merchant.find_by_name(params[:name]) if params[:name]
+          return Merchant.find_by_created_at(params[:created_at]) if params[:created_at]
+          return Merchant.find_by_updated_at(params[:updated_at]) if params[:updated_at]
+        end
+        # rubocop:enable Rails/DynamicFindBy
       end
     end
   end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -18,4 +18,16 @@ class Merchant < ApplicationRecord
   def self.find_all_by_updated_at(updated_at)
     where(updated_at: updated_at)
   end
+
+  def self.find_by_name(name)
+    where('lower(name) LIKE ?', "%#{name.downcase}%").limit(1).first
+  end
+
+  def self.find_by_created_at(created_at)
+    where(created_at: created_at).limit(1).first
+  end
+
+  def self.find_by_updated_at(updated_at)
+    where(updated_at: updated_at).limit(1).first
+  end
 end
