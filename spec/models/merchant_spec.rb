5eec6052b9f55a06c69b9369d4418ad70f345402
Taylor Phillips <taylorscottphillips@gmail.com>
2020-12-16 13:30:56 -0600

    Add test for .find_all_by_name() Merchant model method

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe Merchant, type: :model do
     it { is_expected.to have_many(:invoices) }
     it { is_expected.to have_many(:customers).through(:invoices) }
   end
+
+  describe 'class methods' do
+    describe '.find_all_by_name()' do
+      it 'returns an array of merchants that contain a fragement, case insensitive' do
+        merchant1 = create(:merchant, name: 'Tillman Group')
+        merchant2 = create(:merchant, name: 'Ike Illard')
+        merchant3 = create(:merchant, name: 'Brown, Parker, & Co')
+
+        expect(Merchant.find_all_by_name('ILL')).to eq([merchant1, merchant2])
+      end
+    end
+  end
 end
